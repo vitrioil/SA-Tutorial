fa3f55affaa72fe1d9e3c126dc12088877b1d42e
vitrioil <opm249@gmail.com>
2019-11-07 23:07:30 +0530

    database creation

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import os
 from blog import create_config_app
 
 app = create_config_app()
+
+if not os.path.exists("blog/site.db"):
+    print("Creating database")
+    from blog import db
+    with app.app_context():
+        db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
